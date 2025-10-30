c1de7761a364ea53344680a1fafed9bb517b6f11
Ubuntu <ubuntu@k8s-nova-1.novalocal>
2025-10-30 21:05:45 +0000

    Slight modification to bursty shapes

--- nova_loadtest/load_shapes/bursty_shape.py
+++ nova_loadtest/load_shapes/bursty_shape.py
@@ -6,11 +6,11 @@ class BurstyArrivalShape(LoadTestShape):
 
     bursts = [
         (0, 2000, 60),      # Ramp to 2000 users in 1 minute (start)
-        (300, 150, 60),     # Drop to ~150 users after 5 minutes
+        (300, 100, 60),     # Drop to ~150 users after 5 minutes
         (600, 2000, 60),    # Ramp up again after 10 minutes
-        (900, 150, 60),     # Drop again after 15 minutes
+        (900, 100, 60),     # Drop again after 15 minutes
         (1200, 2000, 60),   # Final ramp up after 20 minutes
-        (1800, 150, 60),    # Final ramp down after 20 minutes
+        (1800, 100, 60),    # Final ramp down after 20 minutes
     ]
     time_limit = 2200
 
